ce8f5d47645130638c6d7b2f96fe42945dc9319e
Igor Davidovich <sir.funtik18@gmail.com>
2020-10-08 15:41:58 +0700

    Setup ui
    add background
    add foreground

---
+++ Main/Quest/Assets/Scripts/Core/GraphMovement/Transitions/Transition.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class Transition : MonoBehaviour {
+
+
+    private Button mainButton;
+
+	private void Awake() {
+		mainButton = GetComponent<Button>();
+	}
+}
