e85760987822582aa98355edc62232708785c0f4
Igor Davidovich <sir.funtik18@gmail.com>
2020-10-08 22:52:00 +0700

    add ReadOnly attribute

---
+++ Main/Quest/Assets/Scripts/Core/InventorySystem/Item/Data/Item.cs
@@ -0,0 +1,17 @@
+﻿using QG.Atributes;
+using UnityEngine;
+
+namespace QG.InventorySystem {
+    public class Item : ScriptableObject {
+        [ReadOnly]
+        [SerializeField]
+        private string itemID = System.Guid.NewGuid().ToString();
+
+        [Header("Information")]
+        public string itemName;
+
+        [TextArea]
+        public string itemDescription;
+
+    }
+}
